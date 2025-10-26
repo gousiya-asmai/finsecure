2b7efeb380976e398c9d80136a4dccce13ce0785
Gousiya-asma <shi95492@gmail.com>
2025-10-26 18:18:12 +0530

    Add Google OAuth login support

--- users/sendgrid_utils.py
+++ users/sendgrid_utils.py
@@ -1,14 +1,14 @@
-import os
 import sendgrid
 from sendgrid.helpers.mail import Mail
+import os
 
 def send_otp_via_sendgrid(email, otp):
-    sg = sendgrid.SendGridAPIClient(api_key=os.getenv("SENDGRID_API_KEY"))
+    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get("SENDGRID_API_KEY"))
     message = Mail(
-        from_email="finsecure7@gmail.com",  # Your verified SendGrid sender email
+        from_email="finsecure7@gmail.com",
         to_emails=email,
         subject="Your Login OTP",
-        plain_text_content=f"Your OTP for login is: {otp}\n\n(Valid for 5 minutes)",
+        plain_text_content=f"Your OTP is: {otp}"
     )
     response = sg.send(message)
-    return response.status_code  # 202 means email accepted by SendGrid
+    return response
