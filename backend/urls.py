cfdc0bef050b1dd3c19ef62a6dbb60ca943116db
Gousiya-asma <shi95492@gmail.com>
2025-10-29 13:26:32 +0530

    Add Google OAuth login support

--- backend/urls.py
+++ backend/urls.py
@@ -13,7 +13,7 @@ from transactions import views as transaction_views
 # ✅ Add for serving media files in development
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import TemplateView
 urlpatterns = [
     # --- Admin ---
     path("admin/", admin.site.urls),
@@ -28,8 +28,10 @@ urlpatterns = [
     path("transactions/", include("transactions.urls")),
     path("dashboard-data/", transaction_views.dashboard_data, name="dashboard_data"),
     path("auth/", include("social_django.urls", namespace="social")),
-
+    path('googlee94c7ab361fbc045.html', TemplateView.as_view(template_name="googlee94c7ab361fbc045.html")),
 ]
+
+
 
 # ✅ Serve media files (profile photos etc.) only in development
 
