8885faa863c593141090aa219d903ccf4fae6509
Gousiya-asma <shi95492@gmail.com>
2025-11-04 16:10:48 +0530

    Remove secrets and add to .gitignore

--- backend/settings.py
+++ backend/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.humanize",
-    "django_crontab",
     "social_django",
     "django_q",
     "users",
